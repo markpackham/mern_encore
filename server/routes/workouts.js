460f33db4ab2810c28d306aab7e88121e2b8f2d1
mark <markpackham1@gmail.com>
2022-07-25 12:33:20 +0100

    add crud routes

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -6,4 +6,20 @@ router.get("/", (req, res) => {
   res.json({ mssg: "GET all workouts" });
 });
 
+router.get("/:id", (req, res) => {
+  res.json({ mssg: "GET a single workout" });
+});
+
+router.post("/", (req, res) => {
+  res.json({ mssg: "POST a new workout" });
+});
+
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "DELETE a workout" });
+});
+
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "UPDATE a workout" });
+});
+
 module.exports = router;
