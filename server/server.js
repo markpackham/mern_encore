1060139db1a4595629c50c3bcb6f6ff8e3e8045d
mark <markpackham1@gmail.com>
2022-07-27 10:51:18 +0100

    resolve cors issue

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 const app = express();
+
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
