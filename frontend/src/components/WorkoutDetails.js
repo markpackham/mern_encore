b3c668d4a4ecbb0a746f1b31b75e771f9be3c5db
mark <markpackham1@gmail.com>
2022-07-28 12:08:06 +0100

    use date fns formatDistanceToNow
    https://youtu.be/Hwsj5iS-rA0?t=289

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,8 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
+// date fns
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
 
@@ -26,7 +29,9 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
       <button onClick={handleClick}>
         Delete <i class="fa-solid fa-trash"></i>
       </button>
