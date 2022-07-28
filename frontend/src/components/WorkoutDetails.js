0f4c62ce201cadcbf93b1d965cbedc8e4c909c2c
mark <markpackham1@gmail.com>
2022-07-28 11:27:15 +0100

    add ability to delete
    https://youtu.be/AWFwioQdjrg?t=2

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,20 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    // since we use proxy in package.json we can avoid the http://localhost:4000 at the start of the fetch
+    const response = await fetch(`/api/workouts/${workout._id}`, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +27,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <button onClick={handleClick}>Delete</button>
     </div>
   );
 };
